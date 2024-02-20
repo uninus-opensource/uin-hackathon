c5c5e1ce2316666fd01fe2dfeea838f7b816199c
sugiartofahmi <sugiartofahmi@gmail.com>
2024-02-20 15:21:10 +0700

    chore: initial api folder structure

--- apps/api/backoffice/src/main.ts
+++ apps/api/backoffice/src/main.ts
@@ -3,20 +3,15 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { TrpcRouter } from '@psu/api';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  app.enableCors();
+  const trpc = app.get(TrpcRouter);
+  trpc.applyMiddleware(app);
+  await app.listen(process.env.PORT || 3000);
 }
-
 bootstrap();
