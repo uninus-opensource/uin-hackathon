44a230602f77d329b4dbcc7a8dc26d486d716beb
sugiartofahmi <sugiartofahmi@gmail.com>
2024-02-24 16:17:52 +0700

    chore: remove cms service

--- libs/api/src/cms/controllers/cms.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { CMSService } from '../services';
-
-@Controller()
-export class CMSController {
-  constructor(private readonly cmsService: CMSService) {}
-}
