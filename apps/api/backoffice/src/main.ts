80935beb7cdc393e0652a47f668cc3be3fb29c71
sugiartofahmi <sugiartofahmi@gmail.com>
2024-02-26 13:42:27 +0700

    chore: add swagger module

--- apps/api/backoffice/src/main.ts
+++ apps/api/backoffice/src/main.ts
@@ -6,10 +6,19 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  const config = new DocumentBuilder()
+    .setTitle('OS HUB API')
+    .setDescription('OS HUB API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
